99a6d8ffb756150bdf76d722a8c3d5419f3c57b1
ashutoshvpujari1-pixel <ashutoshvpujari1@gmail.com>
2026-08-12 19:05:26 +0530

    Add initial project files

---
+++ src/main/java/com/example/springboot_test_app/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springboot_test_app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootTestAppApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/springboot_test_app/WebController.java
@@ -0,0 +1,14 @@
+package com.example.springboot_test_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    @GetMapping("/greet")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello, %s!", name);
+    }
+}
